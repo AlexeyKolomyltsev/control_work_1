a682de832304d23f9896b14e8d5c57eb5c84758f
AlexeyWatt <allexdoma2@gmail.com>
2022-07-02 15:21:37 +0300

    добавил метод количества элементов с длиной меньше 4

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,14 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-void PrintArr(string[] arrayforprint)
+﻿// Создать из массива строк массив строк с элементами длиной меньше либо равной 3
+void PrintArr(string[] arrayforprint) ///метод печати массива
 {
     int len = arrayforprint.Length;
     for (int i = 0; i < len; i++) Console.Write($"{arrayforprint[i]} ");
     Console.WriteLine();
 }
+int CountArray(string[] arrayforcount) /// метод подсчета количества элементов меньше или равное 3
+{
+    int count = 0;
+    foreach (string element in arrayforcount) if (element.Length <= 3) count++;
+    return count;
+}
+
 string[] a = new string[] { "hello", "2", "world", ":-)" };
 string[] b = new string[] { "1234", "1567", "-2", "computer scince" };
 string[] c = new string[] { "Russia", "Denmark", "Kazan" };
 PrintArr(a);
 PrintArr(b);
 PrintArr(c);
-
+Console.WriteLine(CountArray(a));
+Console.WriteLine(CountArray(b));
+Console.WriteLine(CountArray(c));
