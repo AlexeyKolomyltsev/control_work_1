d83d20a0e8c6b7861ef2d22fc2a5f6d1c8312c6e
AlexeyWatt <allexdoma2@gmail.com>
2022-07-02 15:12:08 +0300

    добавил метод печати массива

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void PrintArr(string[] arrayforprint)
+{
+    int len = arrayforprint.Length;
+    for (int i = 0; i < len; i++) Console.Write($"{arrayforprint[i]} ");
+    Console.WriteLine();
+}
+string[] a = new string[] { "hello", "2", "world", ":-)" };
+string[] b = new string[] { "1234", "1567", "-2", "computer scince" };
+string[] c = new string[] { "Russia", "Denmark", "Kazan" };
+PrintArr(a);
+PrintArr(b);
+PrintArr(c);
+
