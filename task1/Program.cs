60da0934218fd2d5c6628667d7875ec4012dca71
AlexeyWatt <allexdoma2@gmail.com>
2022-07-02 15:54:53 +0300

    добавил метод, формирующий новый массив с длиной элементов меньше 4

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,23 +1,44 @@
-﻿// Создать из массива строк массив строк с элементами длиной меньше либо равной 3
+﻿// Создать из массива строк массив строк с элементами длиной меньше 4
+
 void PrintArr(string[] arrayforprint) ///метод печати массива
 {
     int len = arrayforprint.Length;
     for (int i = 0; i < len; i++) Console.Write($"{arrayforprint[i]} ");
     Console.WriteLine();
 }
-int CountArray(string[] arrayforcount) /// метод подсчета количества элементов меньше или равное 3
+
+int CountArray(string[] arrayforcount) /// метод подсчета количества элементов с длиной меньше 4
 {
     int count = 0;
-    foreach (string element in arrayforcount) if (element.Length <= 3) count++;
+    foreach (string element in arrayforcount) if (element.Length < 4) count++;
     return count;
+}
+
+string[] ModifyArray(string[] arrayformodify) /// метод, формирующий новый массив с длиной элементов меньше 4
+{
+    int len = CountArray(arrayformodify);
+    string[] newArray = new string[len];
+    if (len == 0) return newArray;
+    else
+    {
+        int index = 0;
+        foreach (string element in arrayformodify) if (element.Length < 4)
+            {
+                newArray[index] = element;
+                index++;
+            }
+        return newArray;
+    }
 }
 
 string[] a = new string[] { "hello", "2", "world", ":-)" };
 string[] b = new string[] { "1234", "1567", "-2", "computer scince" };
 string[] c = new string[] { "Russia", "Denmark", "Kazan" };
+
 PrintArr(a);
 PrintArr(b);
 PrintArr(c);
-Console.WriteLine(CountArray(a));
-Console.WriteLine(CountArray(b));
-Console.WriteLine(CountArray(c));
+
+PrintArr(ModifyArray(a));
+PrintArr(ModifyArray(b));
+PrintArr(ModifyArray(c));
